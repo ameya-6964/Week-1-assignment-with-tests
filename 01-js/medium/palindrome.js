5c34a9c1ba1bdca56f495e5e97ea0fa6e0b60e8f
ameya-6964 <belvalkar7@gmail.com>
2023-08-31 14:10:47 +0530

    Added Palindrome Code Which Passes All The Test

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -6,8 +6,31 @@
   - `npm run test-palindrome`
 */
 
+function removeSpacesAndSpecialCharacters(str) {
+  let strWithoutSpaces = str.replace(/\s/g, '');
+  let strWithoutSpecialCharacters = strWithoutSpaces.replace(/[^\w\s]/g, '');
+  return strWithoutSpecialCharacters;
+}
+function reverse(str) {
+  var rev = "";
+  for(var i=str.length-1; i>=0; i--) {
+    rev += str[i];
+  }
+  return rev;
+}
+
 function isPalindrome(str) {
-  return true;
+  str = str.toLowerCase();
+  str = removeSpacesAndSpecialCharacters(str); 
+  var rev = reverse(str);
+
+  if(str === rev){
+    return true
+  }
+  else {
+    return false
+  }
+
 }
 
 module.exports = isPalindrome;
